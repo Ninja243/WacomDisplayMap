48449d388fcd49fbb93af1afb0d83ca140ca7650
Mweya Ruider <mweyaruider@gmail.com>
2021-04-10 10:34:51 +0200

    Add files via upload

---
+++ map.sh
@@ -0,0 +1,6 @@
+# Monitor options:
+# 0: +*eDP-1-1 1920/344x1080/194+1920+0  eDP-1-1 	<- Laptop Screen
+# 1: +HDMI-1-2 1920/598x1080/336+3840+0  HDMI-1-2	<- HDMI (left)
+# 2: +DP-1-1 1920/527x1080/296+0+0  DP-1-1		<- VGA (From DP, right)
+xinput map-to-output 14 HDMI-1-2
+xinput map-to-output 15 HDMI-1-2
---
+++ unmap.sh
@@ -0,0 +1,2 @@
+xinput set-prop 14 'Coordinate Transformation Matrix' 1.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0 1.0
+xinput set-prop 15 'Coordinate Transformation Matrix' 1.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0 1.0
